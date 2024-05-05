51edf0ed69b5e48aa0b375354ebacb38564cfd86
kietdev123 <79886528+kietdev123@users.noreply.github.com>
2024-05-05 08:35:45 +0700

    Add project files.

---
+++ LearnDesignPattern/Creational/FactoryMethod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnDesignPattern.Creational
+{
+    class FactoryMethod
+    {
+        public void doExample()
+        {
+            Console.WriteLine("Factory method example");
+        }
+    }
+}
---
+++ LearnDesignPattern/Program.cs
@@ -0,0 +1,19 @@
+﻿using LearnDesignPattern.Creational;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnDesignPattern
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factoryMethod = new FactoryMethod();
+            factoryMethod.doExample();
+        }
+    }
+}
