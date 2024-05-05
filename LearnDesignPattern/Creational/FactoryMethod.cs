e5b8e5afaca19984e81bba0c111eb45bdaaa9f6f
kietdev123 <79886528+kietdev123@users.noreply.github.com>
2024-05-05 09:01:57 +0700

    feat: learn factory method

--- LearnDesignPattern/Creational/FactoryMethod.cs
+++ LearnDesignPattern/Creational/FactoryMethod.cs
@@ -5,12 +5,74 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace LearnDesignPattern.Creational
-{
+{   
+
+    abstract class Job
+    {
+        public abstract void work();
+    }
+
+    class Teacher : Job
+    {
+        public override void work()
+        {
+            Console.WriteLine("Teaching English");
+        }
+    }
+
+    class Developer : Job
+    {
+        public override void work()
+        {
+            Console.WriteLine("Coding...");
+        }
+    }
+
+    interface JobFactory
+    {
+        Job createJob();
+    }
+
+    class TeacherFactory : JobFactory
+    {
+        public Job createJob()
+        {
+            return new Teacher();
+        }
+    }
+
+    class DeveloperFactory : JobFactory
+    {
+        public Job createJob()
+        {
+            return new Developer();
+        }
+    }
+    class Task
+    {
+        public Job job;
+
+        public Task(JobFactory jobFactory)
+        {
+            job = jobFactory.createJob();
+        }
+    }
+
     class FactoryMethod
     {
         public void doExample()
         {
             Console.WriteLine("Factory method example");
+            
+            TeacherFactory teacherFactory = new TeacherFactory();
+            Task teacherTask = new Task(teacherFactory);
+            teacherTask.job.work();
+
+            DeveloperFactory developerFactory = new DeveloperFactory();
+            Task task = new Task(developerFactory);
+            task.job.work();
+
+            Console.ReadLine();
         }
     }
 }
